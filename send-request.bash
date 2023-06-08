c6aa046f9d8220b69a500638e79f192de943a2aa
brig <ybrigo@gmail.com>
2023-06-09 01:07:56 +0200

    up

---
+++ send-request.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+concord_url="$1"
+authToken="$2"
+event="$3"
+payload="$4"
+retries="${5:-3}"
+delay="${6:-5}"
+success=false
+
+options="--http1.1"
+
+for ((attempt=1; attempt<$retries; attempt++)); do
+
+  response=$(curl $options -s -w "%{http_code}" -v \
+    -H "Content-Type: application/json" \
+    -H "Authorization: $authToken" \
+    -d "payload" \
+    -o /dev/null \
+    "$concord_url/api/v1/events/$event")
+
+  status_code="${response: -3}"
+
+  if [[ $status_code == "200" ]]; then
+    echo "Request sent successfully"
+    success=true
+    break
+  else
+    echo "Error sending request. Status code: $status_code"
+    echo "retry #$attempt in $delay seconds"
+    sleep $delay
+  fi
+done
+
+if [[ $success == false ]]; then
+  exit 1
+fi
